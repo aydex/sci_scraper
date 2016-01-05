16ca123f3073a76fed44732085fc66ce7717ba73
Gullern <jangu@stud.ntnu.no>
2016-01-05 21:45:25 +0100

    Small update

--- sci_scraper.py
+++ sci_scraper.py
@@ -2,6 +2,7 @@
 
 ############# Imports ###################
 import requests
+import re
 
 from lang_gen import LangGen
 
@@ -13,8 +14,14 @@ site = "http://physics.nist.gov/cuu/Constants/Table/allascii.txt"
 
 page = requests.get(site)
 text = str(page.text)
-lines = text[text.find('\n', text.find("----------")) + 1 : ].split('\n')
+lines = text[text.find('\n', text.find('----------')) + 1 : ].split('\n')
 print len(lines)
 
+dict = {}
+
+for line in lines:
+    elements = re.split('  +', line)
+    name = elements[0] #ad
+    dict[name] = elements[1:]
 
 
