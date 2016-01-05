7d2efdb4af1f5d6f9a51eda28cf0fad03cd51b8f
Gullern <jangu@stud.ntnu.no>
2016-01-05 21:13:17 +0100

    Abandoned lxml. Revamping sci_scraper.

--- sci_scraper.py
+++ sci_scraper.py
@@ -2,46 +2,17 @@
 
 ############# Imports ###################
 import requests
-from lxml import html
 
 from lang_gen import LangGen
 
 ############# Scientific Scraper ########
 
-site = "http://en.wikipedia.org/wiki/Physical_constant"
-
 gen = LangGen()
 
+site = "http://physics.nist.gov/cuu/Constants/Table/allascii.txt"
+
 page = requests.get(site)
-root = html.fromstring(page.content)
+text = page.text
+print(text)
 
 
-def find_section(node, name):
-    return node.xpath('//h2/span[@id="' + name + '"]')
-
-
-section = find_section(root, "Table_of_universal_constants")[0]
-table = section.getparent().getnext()
-
-
-def scrape_table(node):
-    if node.tag != 'table':
-        raise ValueError('Not a table node')
-    first_row = table.getchildren()[0]
-
-    column_names = pull_table_column_names(first_row)
-
-    for row in table.getchildren()[1:]:
-        for el in row:
-            print el.text
-
-
-def pull_table_column_names(first_row):
-    column_names = []
-    for i in first_row:
-        name = i.text.strip()
-        while name == "" and len(i) > 0:
-            name = i.getchildren()[0].text.strip()
-        if name != "":
-            column_names.append(name)
-    return column_names
